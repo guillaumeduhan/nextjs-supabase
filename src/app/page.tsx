37959bc00409425bf151d0fbdf53ab7509755e21
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-12-10 21:22:54 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import { useArticles } from "@/hooks/useArticles";
+import { supabase } from "@/lib/supabase";
 import { useEffect } from "react";
 
 export default function Home() {
   const { articles, getArticles } = useArticles()
+
+  const subscribedChannel = supabase
+    .channel('articles-follow-up')
+    .on('postgres_changes', {
+      event: '*',
+      schema: 'public',
+      table: 'articles'
+    }, (payload: any) => {
+      console.log(payload)
+    })
+    .subscribe();
+
+  const unsubscribeFromArticles = () => {
+    supabase.removeChannel(subscribedChannel)
+  };
 
   useEffect(() => {
     getArticles()
   }, []);
 
   return <div>
+    <button onClick={() => unsubscribeFromArticles()}>Unsubscribe</button>
     <ul>
       {articles.map((article: any, key: number) => {
         return <li key={key}>{article.title}</li>
